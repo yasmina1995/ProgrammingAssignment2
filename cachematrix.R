1805baecbe8a12975f55278316237d6e6a5ce4af
yasmina1995 <51962437+yasmina1995@users.noreply.github.com>
2019-06-19 11:01:56 +0200

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,21 +1,27 @@
-## Put comments here that give an overall description of what your
-## functions do
+## There are two functions, one called makeCacheMatrix and the other called CacheSolve. You will seee what they
+##both do below
 
-## Write a short comment describing this function
+## First function runs to get the result and the second if the result is already calculates it gets it directly
+##from the cache.
+
+
+##This function creates a special "matrix" object that can cache its inverse.FIrst created a variable called a
+##and set the function. Then assigned x and n in a different environment then the current environment. 
+##The assigned a function to get, setinverse and getinverse, then applied them in a list function.
 
 makeCacheMatrix <- function(x = matrix()) {
-        
- i <- NULL
+  
+  a <- NULL
   set <- function(y) {
     
     x <<- y
-    i <<- NULL
+    a <<- NULL
     
   }
   
   get <- function() x
-  setInverse <- function(inverse) i <<- inverse
-  getInverse <- function() i
+  setInverse <- function(inverse) a <<- inverse
+  getInverse <- function() a
   list(set = set,
        get = get,
        setInverse = setInverse,
@@ -23,21 +29,25 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+##The second function computes the inverse of the special "matrix" returned by "makeCacheMatrix"
+##First created a variable that will get the inverse of x. If the a variable is not null then will get the 
+##cached data and return a from there isn't of running again. If does not find it will find run to get the result
 
 cacheSolve <- function(x, ...) {
+ 
         ## Return a matrix that is the inverse of 'x'
-        
-        cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  a <- x$getInverse()
   
-  i <- x$getInverse()
-  if (!is.null(i)) {
+  if (!is.null(a)) {
+    
     message("getting cached data")
-    return(i)
+    return(a)
+    
   }
+  
   mat <- x$get()
-  i <- solve(mat, ...)
-  x$setInverse(i)
-  i
+  a <- solve(mat, ...)
+  x$setInverse(a)
+  a
+  
 }
